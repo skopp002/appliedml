b4974e7d5065e69e967cfdc66563cb5a52a22140
Sunita Koppar <skopp002@ucr.edu>
2019-04-15 00:27:15 -0700

    Added functions for plotting

--- fibonacci.py
+++ fibonacci.py
@@ -30,8 +30,20 @@ def fibfor(length=0):
         return z
 
 
-def performancechart():
-    nval=10
+def timetrack(n=0):
+    startRec = time.time()
+    fibresult = fibrec(n)
+    print("Recursive response", fibresult)
+    endRec = time.time()
+    print("Recursive method completed")
+    startfor = time.time()
+    forres = fibfor(n)
+    print("Value for for is ", forres)
+    endfor = time.time()
+    print("Time taken for recursive method execution is ", (endRec - startRec))
+    print("Time taken for loop based method is ", (endfor - startfor))
+
+def performancechart(nval=30):
     r1=[]
     f1=[]
     r2=[]
@@ -52,25 +64,15 @@ def performancechart():
 
 
 
-startRec = time.time()
-fibresult = fibrec(10)
-print("Recursive response", fibresult)
-endRec = time.time()
-print("Recursive method completed")
-startfor = time.time()
-forres = fibfor(10)
-print("Value for for is ", forres)
-endfor = time.time()
-print("Time taken for recursive method execution is ", (endRec - startRec))
-print("Time taken for loop based method is ", (endfor-startfor))
-(r1,r2,f1,f2) = performancechart()
-
+timetrack(10)
+nvalForGraph=30
+(r1,r2,f1,f2) = performancechart(nvalForGraph)
 plt.plot(r1,r2)
 plt.show()
-
-x=np.linspace(0,10,10)
+x=np.linspace(0,10,nvalForGraph)
 plt.figure()
 plt.subplot(2,2,1)
+plt.ylabel("timetaken")
 plt.plot(x,r2,'ro')
 plt.title('recursion time taken')
 plt.subplot(2,2,2)
