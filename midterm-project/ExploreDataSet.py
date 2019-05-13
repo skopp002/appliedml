f1c7e2d3b42b6bd895382f89b16544a88b8dd70c
Sunita Koppar <skopp002@ucr.edu>
2019-05-12 19:43:53 -0700

    first commit on the midterm project

---
+++ midterm-project/ExploreDataSet.py
@@ -0,0 +1,4 @@
+# The first step in data analysis is understanding the dataset. Visualization and statistical analysis
+#plays a big role in understanding the data. Lets consider some options to read and understand our dataset
+
+
