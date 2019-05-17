c319fa16533fbfec940f3525982945833b1f85b6
Sunita Koppar <skopp002@ucr.edu>
2019-05-17 09:47:39 -0700

    Adding linear regression trial

--- midterm-project/ExploreDataSet.py
+++ midterm-project/ExploreDataSet.py
@@ -32,13 +32,97 @@
 
 
 import numpy as np
-data = np.load("data/acute_inflammation.tsv", delimiter = ' ', dtype=[('temp',int),
-                                                                            ('nausea',bool),
-                                                                            ('lumbar_pain',bool),
-                                                                            ('freq_urine',bool),
-                                                                            ('micturition_pain',bool),
-                                                                            ('burning',bool),
-                                                                            ('d1_inflammation',bool),
-                                                                            ('d2_nephritis',bool)])
+import pandas as pd
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
 
-data
+
+
+# data = np.load("data/acute_inflammation.tsv", delimiter = ' ', dtype=[('temp',int),
+#                                                                             ('nausea',bool),
+#                                                                             ('lumbar_pain',bool),
+#                                                                             ('freq_urine',bool),
+#                                                                             ('micturition_pain',bool),
+#                                                                             ('burning',bool),
+#                                                                             ('d1_inflammation',bool),
+#                                                                             ('d2_nephritis',bool)])
+# Faced errors using np.load. Hence trying pandas
+
+colNames = ['temp','nausea','lumbar_pain','freq_urine','micturition_pain','burning','d1_inflammation','d2_nephritis']
+data = pd.read_csv("data/acute_inflammation.tsv", sep='\t', header=None)
+data.columns = colNames
+
+'''We can get an idea about individual data elements with below commands'''
+#Histogram
+# fig = plt.figure(figsize=(18,18))
+# ax = fig.gca()
+# data.hist(ax = ax, bins=120)
+# plt.show()
+# or
+#data['nausea'].plot.hist(alpha=0.5,bins=20
+
+temparr = np.array(data['temp'])
+tempscaled = preprocessing.scale(temparr)
+#
+print(tempscaled)
+
+
+'''Trying to get corelation'''
+# data['nausea'] = data['nausea'].map({'yes': 1, 'no': 0})
+# data['d1_inflammation'] = data['d1_inflammation'].str.lower().replace({'yes': 1, 'no': 0})
+# data.plot(kind='bar', x='temp', y='d1_inflammation',color='r')
+# data.plot(kind='bar', x='nausea', y='d1_inflammation',color='g')
+# plt.xlabel('Symptoms')
+# plt.ylabel('Disease')
+# plt.show()
+
+
+
+'''trying to get insights into data
+#Lets start by converting the categorical values to numeric'''
+data.nausea = pd.Categorical(data.nausea, ['yes','no'], ordered = False)
+data.nausea = data.nausea.cat.codes
+data.lumbar_pain = pd.Categorical(data.lumbar_pain, ['yes','no'])
+data.lumbar_pain = data.lumbar_pain.cat.codes
+data.freq_urine = pd.Categorical(data.freq_urine, ['yes','no'])
+data.freq_urine = data.freq_urine.cat.codes
+data.d1_inflammation = pd.Categorical(data.d1_inflammation, ['yes','no'])
+data.d1_inflammation = data.d1_inflammation.cat.codes
+''' So lets start normalizing temp values '''
+data.insert(1, "tempscaled", tempscaled,False)
+plt.plot(data.d1_inflammation,data.tempscaled,'o', color='r')
+plt.plot(data.d1_inflammation,data.nausea,'+',color='g')
+plt.plot(data.d1_inflammation,data.freq_urine,'^',color='b')
+plt.show()
+plt.legend(['tempScaled','nausea'])
+
+
+
+
+'''This is simpler and replaces all categorical in one go. 
+However whether it would assign 1 to yes or 1 to no is pretty adhoc'''
+#df = pd.get_dummies(data)
+'''This shows the effect of skew by using Temp. 
+With this we can understand the need to normalize the temp values'''
+
+#plt.plot(df.d1_inflammation_yes,df.temp,'o', color='r')
+# df.insert(1, "tempscaled", tempscaled,False)
+# df.d1_inflammation_yes,df.d1_inflammation_no)
+# plt.plot(df.d1_inflammation_yes,df.tempscaled,'o', color='r')
+# plt.plot(df.d1_inflammation_yes,df.nausea_yes,'^',color='g')
+# # plt.plot(df.d1_inflammation_yes,df.lumbar_pain_yes,'x',color='b')
+# # plt.plot(df.d1_inflammation_yes,df.freq_urine_yes,'|',color='y')
+# # plt.plot(df.nausea_no,df.d1_inflammation_yes)
+# # plt.plot(df.nausea_no,df.d1_inflammation_no)
+# plt.show()
+# plt.legend(['tempScaled','nausea'])
+
+
+'''Comparison of pandas with sql - https://pandas.pydata.org/pandas-docs/stable/getting_started/comparison/comparison_with_sql.html
+size = count
+'''
+data.groupby('d1_inflammation').agg({'nausea': np.size})
+
+
+
+
---
+++ midterm-project/linearReg.py
@@ -0,0 +1,57 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+model1 = LinearRegression()
+colNames = ['temp','nausea','lumbar_pain','freq_urine','micturition_pain','burning','d1_inflammation','d2_nephritis']
+data = pd.read_csv("data/acute_inflammation.tsv", sep='\t', header=None)
+data.columns = colNames
+
+data.nausea = pd.Categorical(data.nausea, ['yes','no'], ordered = False)
+data.nausea = data.nausea.cat.codes
+data.lumbar_pain = pd.Categorical(data.lumbar_pain, ['yes','no'])
+data.lumbar_pain = data.lumbar_pain.cat.codes
+data.freq_urine = pd.Categorical(data.freq_urine, ['yes','no'])
+data.freq_urine = data.freq_urine.cat.codes
+data.micturition_pain = pd.Categorical(data.micturition_pain, ['yes','no'])
+data.micturition_pain = data.micturition_pain.cat.codes
+data.burning = pd.Categorical(data.burning, ['yes','no'])
+data.burning = data.burning.cat.codes
+data.d1_inflammation = pd.Categorical(data.d1_inflammation, ['yes','no'])
+data.d1_inflammation = data.d1_inflammation.cat.codes
+data.d2_nephritis = pd.Categorical(data.d2_nephritis, ['yes','no'])
+data.d2_nephritis = data.d2_nephritis.cat.codes
+
+''' So lets start normalizing temp values '''
+temparr = np.array(data['temp'])
+tempscaled = preprocessing.scale(temparr)
+data.insert(1, "tempscaled", tempscaled,False)
+
+X = data.drop(['temp','d1_inflammation','d2_nephritis'],axis="columns")
+print(X)
+
+y1 = data.d1_inflammation
+
+print("X and y1" , X, y1)
+y2 = data.d2_nephritis
+model1.fit(X,y1)
+model1.predict([[1.5,1,1,0,0,1]])
+
+model2 = LinearRegression()
+model2.fit(X,y2)
+model2.predict([[1.5,1,1,0,0,1]])
+
+model1.score(X,y1)
+
+
+
+'''Lets visualize using seaborn'''
+plt.figure()
+sns.distplot(data,X,y1)
+#v1 = sns.lmplot(x='nausea', y='d2_nephritis', data = data)
+plt.show
+
